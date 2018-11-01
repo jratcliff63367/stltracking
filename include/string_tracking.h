8f239e081884e7a765b4d9d8427590bf15f2be27
jratcliff <jratcliff@nvidia.com>
2018-11-01 16:01:02 -0600

    Putting in stringtest

--- include/string_tracking.h
+++ include/string_tracking.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#include "stl_tracking.h"
+
+template <class CharT, class Traits = std::char_traits<CharT>>
+using basic_string = std::basic_string<CharT, Traits, tracking_allocator<CharT>>;
+
+using string = basic_string<char>;
