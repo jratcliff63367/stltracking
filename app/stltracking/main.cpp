d89a1531a152675f58e3818b76ba2387f2f22c85
jratcliff <jratcliff@nvidia.com>
2018-11-01 16:01:02 -0600

    Putting in stringtest

--- app/stltracking/main.cpp
+++ app/stltracking/main.cpp
@@ -4,18 +4,36 @@
 #include <stdint.h>
 
 #include "vector_tracking.h"
+#include "string_tracking.h"
 
 void exampleTrackingVector(void)
 {
+    printf("==============================\n");
+    printf("ExampleTrackingVector\n");
+    printf("==============================\n");
     vector<uint32_t> vec;
     for (uint32_t i = 0; i < 10; i++)
     {
         vec.push_back(i);
     }
+    printf("==============================\n");
+    printf("\n");
+}
+
+void exampleTrackingString(void)
+{
+    printf("==============================\n");
+    printf("ExampleTrackingString\n");
+    printf("==============================\n");
+    string s("This is a test string");
+    s += "more data";
+    printf("==============================\n");
+    printf("\n");
 }
 
 int main(int /* argc */,const char ** /* argv */)
 {
     exampleTrackingVector();
+    exampleTrackingString();
 	return 0;
 }
